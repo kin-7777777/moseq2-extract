9ae12b68f01abf39ddc66745461e305754bbec20
Makinolab <makinolab>
2023-07-04 15:17:30 +0800

    Minor fix

--- moseq2_extract/extract/proc.py
+++ moseq2_extract/extract/proc.py
@@ -461,15 +461,17 @@ def get_frame_features(frames, frame_threshold=10, mask=np.array([]),
                 moment_feats = im_moment_features(cnts[mouse_cnt])
                 mice_last_centroids.append(moment_feats['centroid'])
                 mice_last_orientations.append(moment_feats['orientation'])
+            mice_last_centroids = np.array(mice_last_centroids)
+            mice_last_orientations = np.array(mice_last_orientations)
         
         for k in range(number_of_mice):
             mouse_cnt = mouse_cnts[k]
             # Get features from contours
             moment_feats = im_moment_features(cnts[mouse_cnt])
             # Now we need to match the mice identities based on the features from the previous frame.
-            centroid_distance_scores = scipy.linalg.norm(mice_last_centroids - moment_feats['centroid'], axis=1) # the lower, the closer
+            centroid_distance_scores = scipy.linalg.norm(mice_last_centroids - np.array(moment_feats['centroid']), axis=1) # the lower, the closer
             centroid_distance_scores = centroid_distance_scores / np.max(centroid_distance_scores) # normalize to between 0 and 1
-            orientation_distance_scores = np.cos(mice_last_centroids - moment_feats['orientation']) # the higher, the closer
+            orientation_distance_scores = np.cos(mice_last_centroids - np.array(moment_feats['orientation'])) # the higher, the closer
             similarity_score = orientation_distance_scores - centroid_distance_scores
             id = np.max(similarity_score)
             
