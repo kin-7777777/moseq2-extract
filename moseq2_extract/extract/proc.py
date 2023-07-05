e0755197c1e96be8578fb502aa9049c52e1b38fc
Makinolab <makinolab>
2023-07-05 13:06:39 +0800

    Minor fix

--- moseq2_extract/extract/proc.py
+++ moseq2_extract/extract/proc.py
@@ -483,10 +483,10 @@ def get_frame_features(frames, frame_threshold=10, mask=np.array([]),
                 # Now we need to match the mice identities based on the features from the previous frame.
                 centroid_distance_scores = scipy.linalg.norm(mice_last_centroids - np.array(moment_feats['centroid']), axis=1) # the lower, the closer
                 centroid_distance_scores = centroid_distance_scores / np.max(centroid_distance_scores) # normalize to between 0 and 1
-                orientation_distance_scores[k] = np.cos(mice_last_orientations - np.array(moment_feats['orientation'])) # the higher, the closer
+                orientation_distance_scores[k, :] = np.cos(mice_last_orientations - np.array(moment_feats['orientation'])) # the higher, the closer
+                print(orientation_distance_scores)
                 similarity_scores = -centroid_distance_scores
                 id = np.argmax(similarity_scores)
-                print(similarity_scores)
                 assigned_ids.append(id)
             duplicated_ids = [item for item, count in Counter(assigned_ids).items() if count > 1]
             print("duplicated_ids: "+str(duplicated_ids))
@@ -496,14 +496,14 @@ def get_frame_features(frames, frame_threshold=10, mask=np.array([]),
                 new_assigned_ids = assigned_ids
                 for dup_id in duplicated_ids:
                     culprits = np.squeeze(np.argwhere(assigned_ids == dup_id))
-                    print(culprits)
+                    print("culprits: "+str(culprits))
                     cost_matrix = np.zeros((len(culprits), len(culprits)))
                     print(cost_matrix)
                     for l in range(len(culprits)):
                         for m in range(len(culprits)):
                             cost_matrix[l, m] = -orientation_distance_scores[culprits[l], culprits[m]]
                     _, assignment = scipy.optimize.linear_sum_assignment(cost_matrix)
-                    print(assignment)
+                    print("assignment: "+str(assignment))
                     for l in range(len(culprits)):
                         new_assigned_ids[culprits[l]] = culprits[assignment[l]]
                 assigned_ids = new_assigned_ids
